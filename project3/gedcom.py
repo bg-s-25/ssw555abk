2f74e785ffb04b92806237881bfc5289d16110cc
bg-s-25 <hgeorgio@stevens.edu>
2020-02-07 11:31:24 -0500

    Pretty table testing

--- project3/gedcom.py
+++ project3/gedcom.py
@@ -3,20 +3,53 @@
 '''
 
 import sys
+from prettytable import PrettyTable
 
 tags = {0: ['INDI', 'FAM', 'HEAD', 'TRLR', 'NOTE'],
         1: ['NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV'],
         2: ['DATE']}
 
-def process(block):
+def print_collection(col):
+    x = PrettyTable()
+    x.field_names = ["Id", "Name", "Sex", "c", "d", "e", "f", "g", "h"]
+    for indi_id in col:
+        x.add_row([indi_id, col[indi_id][1], col[indi_id][2], '', '', '', '', '', ''])
+    print(x)
+
+def process(valid_lines):
     individuals = {}
     families = {}
+    current = ('', '') # current type of collection & current id, e.g. ('INDI', 'I22')
+
+    for line in valid_lines:
+        lvl, tag, args = line
+        if lvl == 0 and tag == 'INDI':
+            # new individual
+            current = ('INDI', args[0].strip('@'))
+            individuals[current[1]] = [''] * 9
+            individuals[current[1]][0] = current[1]
+
+        elif lvl == 0 and tag == 'FAM':
+            # new family
+            pass
+
+        elif current[0] == 'INDI':
+            # continue processing individual
+            if tag == 'NAME':
+                individuals[current[1]][1] = ' '.join(args)
+            elif tag == 'SEX':
+                individuals[current[1]][2] = args[0]
+
+        elif current[0] == 'FAM':
+            # continue processing family
+            pass
+            
+    print_collection(individuals)
 
 # print each line and separate the level, tag, arguments and tell if it is valid
 def check_valid(lines):
-    cur_block = []
+    valid_lines = []
     for line in lines:
-        
         line = line.split()
         valid = True
 
@@ -40,15 +73,14 @@ def check_valid(lines):
         # valid tag/level?
         if valid and not tag in tags[lvl]: valid = False
 
-        # if new individual/family, process the current block; otherwise, append to the current block
-        if valid and lvl == 0:
-            process(cur_block)
-            cur_block += [lvl, tag, args]
-        elif valid:
-            cur_block += [lvl, tag, args]
+        # if line is valid, add it to valid lines to be processed
+        if valid: valid_lines += [[lvl, tag, args]]
         
         # print result
         # print('<-- ' + str(lvl) + '|' + tag + '|' + (lambda val: 'Y' if val else 'N')(valid) + '|' + args)
+    
+    # finally, pass the valid lines to be processed
+    process(valid_lines)
 
 # entry point
 if len(sys.argv) != 2:
