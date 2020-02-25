3b90360aea0968064cde3d6818488baf8c92ce2e
bg-s-25 <hgeorgio@stevens.edu>
2020-02-25 15:13:18 -0500

    Changed us21 & us22 error message

--- project3/testing/us21.py
+++ project3/testing/us21.py
@@ -12,7 +12,7 @@ def verify_correct_roles(indivs, fams):
         husb_id = fams[fam_id][3]
         wife_id = fams[fam_id][5]
         if indivs[husb_id][2] != 'M':
-            return (False, "ERROR: INDIVIDUAL: US21: X: {}: Incorrect role for husband in family {}".format(husb_id, fam_id))
+            return (False, "ERROR: INDIVIDUAL: US21: {}: Incorrect role for husband in family {}".format(husb_id, fam_id))
         if indivs[wife_id][2] != 'F':
-            return (False, "ERROR: INDIVIDUAL: US21: X: {}: Incorrect role for wife in family {}".format(wife_id, fam_id))
+            return (False, "ERROR: INDIVIDUAL: US21: {}: Incorrect role for wife in family {}".format(wife_id, fam_id))
     return (True, "")
--- project3/testing/us22.py
+++ project3/testing/us22.py
@@ -10,8 +10,8 @@ import gedcom
 def verify_unique_ids(indivsLst, famsLst):
     for indiv_id in indivsLst:
         if indivsLst.count(indiv_id) > 1:
-            return (False, "ERROR: INDIVIDUAL: US22: X: {}: Multiple occurrences of individual's id".format(indiv_id))
+            return (False, "ERROR: INDIVIDUAL: US22: {}: Multiple occurrences of individual's id".format(indiv_id))
     for fam_id in famsLst:
         if famsLst.count(fam_id) > 1:
-            return (False, "ERROR: FAMILY: US22: X: {}: Multiple occurrences of family's id".format(fam_id))
+            return (False, "ERROR: FAMILY: US22: {}: Multiple occurrences of family's id".format(fam_id))
     return (True, "")
