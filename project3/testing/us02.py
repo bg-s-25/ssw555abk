e2317f8546e42b21157470777414a3931c1d8e5c
uasif13 <uasif13@gmail.com>
2020-02-27 18:24:31 -0500

    small mistake in us02

--- project3/testing/us02.py
+++ project3/testing/us02.py
@@ -16,12 +16,12 @@ def bbm(pers, fam):
     # errors will be a list of all the inconsistent entries
     # Every marriage has a husband and a wife
     errors = []
-    for value in fam:
-        hus_id = value[3]
-        wife_id = value[5]
-        if (pers[hus_id][3] >= value[1]):
+    for key in fam:
+        hus_id = fam[key][3]
+        wife_id = fam[key][5]
+        if (pers[hus_id][3] >= fam[key][1]):
             errors += ["ERROR: FAMILY: US02: {}: Husband's birth date {} after marriage date {}".format(hus_id,pers[hus_id][3],value[1])]
-        if (pers[wife_id][3] >= value[1]):
+        if (pers[wife_id][3] >= fam[key][1]):
             errors += ["ERROR: FAMILY: US02: {}: Wife's birth date {} following marriage date {}".format(hus_id,pers[wife_id][3],value[1])]
     return [False] + errors if len(errors) > 0 else [True] + errors
 
