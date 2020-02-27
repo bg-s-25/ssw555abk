5126f6ebe8d645747c28519faf8c80abbe807b65
uasif13 <uasif13@gmail.com>
2020-02-27 18:52:57 -0500

    Revert "fixed us02 with errors"
    This reverts commit e2906a8f45b2337fa9bec9cd5afe8d0ad88d1798.

--- project3/testing/us02_test.py
+++ project3/testing/us02_test.py
@@ -10,6 +10,7 @@ class Tests(unittest.TestCase):
 
     def test01(self):
         individuals, families = gedcom.tester(self.gedfiles[0])[:2]
+        print(us02.bbm(individuals,families))
         self.assertEqual(us02.bbm(individuals, families)[0], True)
 
 
