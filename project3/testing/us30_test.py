634eb72385e473597ef4a647ff6c5db853308f41
kburns2 <kburns2@stevens.edu>
2020-02-28 15:11:49 -0500

    finished us30

--- project3/testing/us30_test.py
+++ project3/testing/us30_test.py
@@ -5,20 +5,20 @@ sys.path.insert(0, '../')
 import gedcom
 
 class Tests(unittest.TestCase):
-    
-    gedfiles = ['us30_01.ged','us30_02.ged','us30_03.ged']
+    gedfiles = ['us30_01.ged','us30_02.ged']
+    txtfiles = ['us30_01.txt','us30_02.txt']
 
-    def test01(self): #married
+    def test01(self): 
         indivs, fams = gedcom.tester(self.gedfiles[0])[:2]
-        self.assertEqual(us30.listmarried(indivs, fams), True)
-
-    def test02(self): #married and divorced(not dead)
+        test_file = us30.listmarried(indivs, fams)
+        result_file = self.txtfiles[0]
+        self.assertEqual(us30.compare(test_file, result_file)[0], True)
+    
+    def test02(self): 
         indivs, fams = gedcom.tester(self.gedfiles[1])[:2]
-        self.assertEqual(us30.listmarried(indivs, fams), True)
-
-    def test03(self): #married and divorced(not dead)
-        indivs, fams = gedcom.tester(self.gedfiles[2])[:2]
-        self.assertEqual(us30.listmarried(indivs, fams), True)
+        test_file = us30.listmarried(indivs, fams)
+        result_file = self.txtfiles[1]
+        self.assertEqual(us30.compare(test_file, result_file)[0], False)
 
 def tester():
     unittest.main()
