6fa39802e726843d7fe5dd9f5684c94e10406667
uasif13 <uasif13@gmail.com>
2020-03-31 11:59:16 -0400

    added functionality to us04.py - still need to add tests

--- testing/sprint3/us04.py
+++ testing/sprint3/us04.py
@@ -7,8 +7,13 @@ sys.path.insert(0, '../sprint2')
 import main_parser
 
 '''
-    Given individuals that have married and divorced, ensure that marriage occurs before divorce and divorce occurs after marriage
+    Given individuals, ensure that marriage occurs before divorce and divorce occurs after marriage
 '''
 
-def marriage_before_divorce():
-    
+def marriage_before_divorce(families):
+    errors = []
+    for family in families: # parse through each family
+        #check to see if the family has divorced and check to see if divorce occurred before or on the day of marriage
+        if (families[2] != 'NA' and families[2] <= families[1]): 
+            errors += ['']
+    return errors
