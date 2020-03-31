07b09337e4bac160f064e269c339ee4c5e5ce4ce
uasif13 <uasif13@gmail.com>
2020-03-31 10:46:15 -0400

    created the *.py and *test.py for sprint3

---
+++ testing/sprint3/us04.py
@@ -0,0 +1,10 @@
+# US04: Marriage before divorce
+
+import sys
+sys.path.insert(0, '../../')
+sys.path.insert(0, '../sprint1')
+sys.path.insert(0, '../sprint2')
+import main_parser
+
+def marriage_before_born():
+    
---
+++ testing/sprint3/us04_test.py
@@ -0,0 +1,9 @@
+import sys
+import unittest
+import us36
+sys.path.insert(0, '../')
+sys.path.insert(0, '../../')
+import git_utils
+import compare
+import main_parser
+
